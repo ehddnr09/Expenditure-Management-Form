1f410ee462f5808a3ee0ccda0bb6db7f89640355
ehddnr09 <ilovpark@naver.com>
2022-08-29 22:07:01 +0900

    add ExpenseForm file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
-// import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
   const expenses = [
@@ -26,7 +28,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
